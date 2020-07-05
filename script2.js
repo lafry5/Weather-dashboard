cad113d3d125796f208cb9d1c05d3bb04dcf0065
lafry5 <lafry5@yahoo.com>
2020-07-05 08:49:13 -0700

    finished 5 day forecast

--- script2.js
+++ script2.js
@@ -7,28 +7,31 @@ var temp = document.querySelector('#temp')
 var uv = document.querySelector('#uv')
 var desc = document.querySelector('#desc')
 var day = document.querySelector('#day')
-var city = ''; 
+
+var cities = {}
 
 day = moment().format('L');
-
-var cities = {};
 
 var loadCities = function() {
     cities = JSON.parse(localStorage.getItem("cities"));
   }
 
-//if nothing in local Storage, create a new object to track all cities arrays
-
 if (!cities)  {
     cities = [];
 };
 
+//if nothing in local Storage, create a new object to track all cities arrays
 
 // Need help below this...
-var saveCities = function() { //stores to local storage (setItem, getItem)
+var saveCities = function() { //stores to local storage 
    console.log('executed save cities')
    localStorage.setItem("cities", JSON.stringify(cities));
 };
+
+
+
+
+
 
 function getSearchVal() {
         var searchValue = document.querySelector("#search-value").value;
@@ -63,6 +66,7 @@ btn.addEventListener('click', function() {
 function searchcities(city){
        let queryurl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=8b58730b831d9dfe90f82c5fd73e1a99"
        var CityInfo = [];
+       day = moment().format('L');
        var name = document.getElementById('name') //temp test
        var humidity = document.getElementById('humidity')
        var temp = document.getElementById('temp')
@@ -70,6 +74,21 @@ function searchcities(city){
        var uv = document.getElementById('uv')
        var day = document.getElementById('day') 
        var desc = document.querySelector('#desc')
+       var day1 = document.getElementById('day1')
+       var humidity1 = document.getElementById('humidity1')
+       var temp1 = document.getElementById('temp1')
+       var day2 = document.getElementById('day1')
+       var humidity2 = document.getElementById('humidity2')
+       var temp2 = document.getElementById('temp2')
+       var day3 = document.getElementById('day3')
+       var humidity3 = document.getElementById('humidity3')
+       var temp3 = document.getElementById('temp3')
+       var day4 = document.getElementById('day4')
+       var humidity4 = document.getElementById('humidity4')
+       var temp4 = document.getElementById('temp4')
+       var day5 = document.getElementById('day5')
+       var humidity5 = document.getElementById('humidity5')
+       var temp5 = document.getElementById('temp5')
        fetch(queryurl).then(function(response){
        return response.json()
        }).then(function(response){
@@ -80,13 +99,15 @@ function searchcities(city){
        // need to add name to the cities list 
        console.log(response.name)
        day.innerHTML = moment().format('L');
+      
+     
        // desc.innerHTML = response.weather.main
        //console.log(response.weather.main)
 
        //Get current temperature
        temp.innerHTML = "Temperature is " + response.main.temp + " C" 
        console.log(response.main.temp)
-     
+       
        //Humidity
        humidity.innerHTML = "Humidity is " + response.main.humidity + "%"
        console.log(response.main.humidity)
@@ -97,43 +118,67 @@ function searchcities(city){
        console.log(response.wind.speed)
      
         // UV variable
-            uvfunct(response.coord.lat, response.coord.lon)
+        uvfunct(response.coord.lat, response.coord.lon)
 
         //weather icon; need icon id
         // iconfunct(icon)
-//       })
-//    } 
-}) //end of searchcities function
+       })  .catch(function(err) {
+            console.log('error')
+     })
+    }
+//} //end of searchcities function
 
 
-// UV function
+
+// UV function & forecast
 var uvfunct = function(lat,lon) {
    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid=8b58730b831d9dfe90f82c5fd73e1a99"
+   day = moment().format('L');
    fetch(apiUrl).then(function(result) {
         console.log(result)
         console.log('here')
         return result.json();
     }).then(function(result) {
-        console.log(result.current.uvi)
+        console.log(result)
         uv.innerHTML = "UV index is " + result.current.uvi;
+        dayone = moment(). add(1,'days').format('L');
+        daytwo = moment(). add(2,'days').format('L');
+        daythree = moment(). add(3,'days').format('L');
+        dayfour = moment(). add(4,'days').format('L');
+        dayfive = moment(). add(5,'days').format('L');
+        day1.innerHTML = dayone;
+        day2.innerHTML = daytwo;
+        day3.innerHTML = daythree;
+        day4.innerHTML = dayfour;
+        day5.innerHTML = dayfive;
+        temp1.innerHTML = "Temp: " + result.daily[1].temp.day + " C"
+        temp2.innerHTML = "Temp: " + result.daily[2].temp.day + " C"
+        temp3.innerHTML = "Temp: " + result.daily[3].temp.day + " C"
+        temp4.innerHTML = "Temp: " + result.daily[4].temp.day + " C"
+        temp5.innerHTML = "Temp: " + result.daily[5].temp.day + " C"
+        humidity1.innerHTML = "Humidity: " + result.daily[1].humidity + "%"
+        humidity2.innerHTML = "Humidity: " + result.daily[2].humidity + "%"
+        humidity3.innerHTML = "Humidity: " + result.daily[3].humidity + "%"
+        humidity4.innerHTML = "Humidity: " + result.daily[4].humidity + "%"
+        humidity5.innerHTML = "Humidity: " + result.daily[5].humidity + "%"
     })
-    }
-}
+   }
+//} // runs without it ?!
 
 
 // Weather icon function
-var iconfunct = function(icon) {
-   var apiUrl = "http://openweathermap.org/img/w/"+icon+".png";
-    fetch(apiUrl).then(function(ending) {
-         console.log(ending)
-         console.log('finally')
-         return result.json();
-     }).then(function(ending) {
-         //weather icon
-         console.log(ending.weather.icon)
-         $(".icon").html("<img src=' + .icon + '>") //Doesn't print an image
-         desc.innerHTML = ending.weather.icon;      //Doesn't print
-     })
-     }
-// } //this seems to be needed based on uvfunction, but runs without it ?!
+//var iconfunct = function(icon) {
+//   var apiUrl = "http://openweathermap.org/img/w/"+icon+".png";
+//    fetch(apiUrl).then(function(ending) {
+//         console.log(ending)
+//         console.log('finally')
+//         return result.json();
+//     }).then(function(ending) {
+//         //weather icon
+//         console.log(ending.weather.icon)
+//         $(".icon").html("<img src=' + .icon + '>") //Doesn't print an image
+//         desc.innerHTML = ending.weather.icon;      //Doesn't print
+//     })
+//     }
+// } // runs without it ?!
 
